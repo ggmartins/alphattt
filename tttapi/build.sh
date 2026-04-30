7e7e298b60cc3a55e7eddb15ca9ea9bf589fd67b
ggmartins <ggmartins@gmail.com>
2026-04-29 22:04:36 -0500

    fix build.sh force install

--- tttapi/build.sh
+++ tttapi/build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env bash
 
-docker build -t my-app:v0 .
+APP=my-app:v0
+
+docker build -t $APP .
+for node in $(docker ps --format '{{.Names}}' | grep '^desktop-'); do
+  echo "Loading image into $node"
+  docker save $APP | docker exec -i "$node" ctr -n k8s.io images import -
+done
