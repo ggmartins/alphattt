346b30af4624ea2df3f328d2e7bd45a04846280f
ggmartins <ggmartins@gmail.com>
2026-04-29 21:59:11 -0500

    fix check build sh + webapp1 -> tttback

--- tttapi/check.sh
+++ tttapi/check.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 #docker scout cves --details my-app:latest
 docker scout cves --only-severity critical,high my-app:latest
 docker scout recommendations my-app:latest
