82462fb37e877b22d22f0e720f860873a3ccc7e1
ggmartins <ggmartins@gmail.com>
2026-05-01 00:20:20 -0500

    fix: spec warnings

--- tttfront/src/components/sessionstatus.component.spec.ts
+++ tttfront/src/components/sessionstatus.component.spec.ts
@@ -1,3 +1,4 @@
+import 'jasmine';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { SessionStatusComponent } from './sessionstatus.component';
 import { SessionStatus } from './sessionstatus.model';
