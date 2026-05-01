8edcb85393abb6c23710f7f33ff1c2046f7cae24
ggmartins <ggmartins@gmail.com>
2026-05-01 04:21:54 -0500

    fix: not your turn

--- tttfront/src/components/sessionstatus.model.ts
+++ tttfront/src/components/sessionstatus.model.ts
@@ -3,6 +3,7 @@ export type MatchStatus = 'finished' | 'ongoing' | 'not_launched';
 export interface SessionStatus {
   sessionId: number;
   vsplayer: String;
+  playerId: number;
   timestamp: Date;
   board: [ [null, null, null], [null, null, null], [null, null, null] ];
   playingAs: 'X' | 'O';
